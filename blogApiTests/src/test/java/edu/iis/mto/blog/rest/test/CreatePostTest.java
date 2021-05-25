79a69570073dce78116065e7a037efdac8758165
wiktorowski.michal <222609@edu.p.lodz.pl>
2021-05-25 16:30:24 +0200

    3.4.2 added post test, only user with confirmed status can add post

---
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreatePostTest.java
@@ -0,0 +1,45 @@
+package edu.iis.mto.blog.rest.test;
+
+import io.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class CreatePostTest extends FunctionalTests {
+
+    private static final String USER_POST_API = "/blog/user/{userId}/post";
+
+    @Test
+    void createPostWithConfirmedStatusShouldResponseWithCreatedStatus() {
+        long idUserConfirmed = 1L;
+
+        JSONObject jsonObj = new JSONObject().put("entry", "entry content");
+        given().accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString())
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_CREATED)
+                .when()
+                .post(USER_POST_API, idUserConfirmed);
+    }
+
+    @Test
+    void createPostWithConfirmedStatusShouldResponseWithBadRequestStatus() {
+        long isUserNotConfirmed = 2L;
+        JSONObject jsonObj = new JSONObject().put("entry", "entry content");
+        given().accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString())
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_BAD_REQUEST)
+                .when()
+                .post(USER_POST_API, isUserNotConfirmed);
+    }
+
+}
